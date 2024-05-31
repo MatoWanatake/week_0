e5fcd65649039a381320ed2392f02fd7425a44d4
Michael Brandon <mikebrandon91@gmail.com>
2024-05-30 19:32:54 -0500

    first commit

---
+++ problems/02-code-comments.js
@@ -0,0 +1,27 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log('Welcome to App Academy!');
+
+/* Print more messages to the terminal using console.log */
+
+// 2. This is a single line comment.
+
+
+// console.log('this wont be seen either');
+
+//console.log('What it do?');
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+
+// Your code here
+
+/* a
+multiline
+comment */
